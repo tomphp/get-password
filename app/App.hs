1a01a596930b44314825f13f56f7213b24d4257d
Tom Oram <me@tomoram.io>
2023-02-09 18:26:55 +0000

    refactor: Remove Args module

--- app/App.hs
+++ app/App.hs
@@ -1,31 +1,28 @@
 module App (app) where
 
-import qualified Args
 import ConfigLoader.Class (LoadConfigError, MonadConfigLoader)
 import qualified ConfigLoader.Class as ConfigLoader
 import ConfigLoader.Config (Config (Config, user))
-import Console.Class (MonadConsole)
-import Control.Monad.Error.Class (liftEither)
-import Control.Monad.Except (MonadError, runExceptT)
-import Control.Monad.IO.Class (MonadIO (liftIO))
+import Control.Monad.Error.Class (MonadError (throwError), liftEither)
+import Control.Monad.Except (runExceptT)
+import Control.Monad.IO.Class (MonadIO (liftIO), liftIO)
 import qualified Data.Bifunctor as Bifunctor
-import Data.Either.Extra (maybeToEither)
 import Data.Text (Text)
 import qualified Data.Text as Text
 import GetPassword (GetPasswordError (..))
 import qualified GetPassword
 import LastPass.Class (MonadLastPass, Password)
-import LastPass.Entry (Search)
+import LastPass.Entry (Search (Search))
 import Printer.Class (MonadPrinter (printGetPasswordError, printLoadConfigError, printPassword, printUsage))
 import qualified System.Environment as Env
 import System.Exit (exitFailure)
 
 data AppError
-  = AppGetArgsError
+  = AppGetArgsError Text
   | AppLoadConfigError LoadConfigError
   | AppGetPasswordError GetPasswordError
 
-app :: (MonadIO m, MonadPrinter m, MonadConsole m, MonadConfigLoader m, MonadLastPass m) => m ()
+app :: (MonadIO m, MonadPrinter m, MonadConfigLoader m, MonadLastPass m) => m ()
 app = do
   result <- runExceptT $ do
     search <- getArgs
@@ -38,7 +35,14 @@ app = do
     Left err -> printError err >> liftIO exitFailure
 
 getArgs :: (MonadError AppError m, MonadIO m) => m Search
-getArgs = Args.getArgs >>= wrapError (const AppGetArgsError) . maybeToEither ()
+getArgs = do
+  args <- liftIO Env.getArgs
+  maybe throwGetArgsError return (parseArgs args)
+  where
+    parseArgs [search] = Just $ Search $ Text.pack search
+    parseArgs _ = Nothing
+
+    throwGetArgsError = throwError . AppGetArgsError =<< getProgName
 
 loadConfig :: (MonadError AppError m, MonadConfigLoader m) => m Config
 loadConfig = ConfigLoader.loadConfig >>= wrapError AppLoadConfigError
@@ -47,13 +51,13 @@ getPassword :: (MonadError AppError m, MonadLastPass m) => Config -> Search -> m
 getPassword Config {user} search =
   runExceptT (GetPassword.getPassword user search) >>= wrapError AppGetPasswordError
 
-printError :: (MonadPrinter m, MonadConsole m, MonadIO m) => AppError -> m ()
-printError AppGetArgsError = getProgName >>= printUsage
+printError :: MonadPrinter m => AppError -> m ()
+printError (AppGetArgsError progName) = printUsage progName
 printError (AppLoadConfigError err) = printLoadConfigError err
 printError (AppGetPasswordError err) = printGetPasswordError err
 
 getProgName :: MonadIO m => m Text
 getProgName = Text.pack <$> liftIO Env.getProgName
 
-wrapError :: (MonadError e' m) => (e -> e') -> Either e a -> m a
+wrapError :: MonadError e' m => (e -> e') -> Either e a -> m a
 wrapError f = liftEither . Bifunctor.first f
