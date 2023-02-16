980df46d8a350ec4380fcb97955bd5a9b48db6de
Tom Oram <me@tomoram.io>
2023-02-16 22:22:08 +0000

    refactor: Extract getMatchingEntries

--- src/GetPassword.hs
+++ src/GetPassword.hs
@@ -3,6 +3,7 @@ module GetPassword (getPassword) where
 import Control.Monad (void)
 import Control.Monad.Except (MonadError (throwError))
 import Data.Text (Text)
+import Entry (Entry)
 import qualified Entry
 import LastPass (LastPassError (LastPassMultiplePasswordsFound, LastPassPasswordNotFound), MonadLastPass)
 import qualified LastPass
@@ -12,9 +13,12 @@ getPassword search = do
   void LastPass.checkIsInstalled
   void LastPass.checkIsLoggedIn
 
-  results <- filter (Entry.matches search) <$> LastPass.listPasswords
+  results <- getMatchingEntries search
 
   case results of
     [] -> throwError LastPassPasswordNotFound
     [entry] -> LastPass.showPassword (Entry.id entry)
     _ -> throwError (LastPassMultiplePasswordsFound results)
+
+getMatchingEntries :: (MonadLastPass m, MonadError LastPassError m) => Text -> m [Entry]
+getMatchingEntries search = filter (Entry.matches search) <$> LastPass.listPasswords
