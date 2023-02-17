2aa693f46f93f788a2807afc75eac1beb681635d
Tom Oram <me@tomoram.io>
2023-02-17 23:27:08 +0000

    feat: Print helpful error messages

--- app/Main.hs
+++ app/Main.hs
@@ -6,9 +6,12 @@ import Data.Text (Text)
 import qualified Data.Text as Text
 import qualified Data.Text.IO as TextIO
 import qualified GetPassword
-import GetPassword (GetPasswordError)
+import GetPassword (GetPasswordError (..))
 import qualified LastPass
 import qualified System.Environment as Env
+import qualified LastPass.Entry as Entry
+import LastPass (LastPassError (..))
+import System.IO (stderr)
 
 main :: IO ()
 main = getArgs >>= maybe printUsage runApp
@@ -30,9 +33,24 @@ printPassword :: Text -> IO ()
 printPassword = TextIO.putStrLn
 
 printError :: GetPasswordError -> IO ()
-printError err = TextIO.putStrLn ("Error: " <> Text.pack (show err))
+printError PasswordNotFound = putErrorLn "Error: No matching entries found"
+printError (MultiplePasswordsFound entries) = do
+  putErrorLn "Error: Multiple entries found:"
+  putErrorLn "Matching entries:"
+  mapM_ (\entry -> putErrorLn (" - " <> Entry.name entry <> " [" <> Entry.id entry <> "]")) entries
+printError (LastPassErrored err) = printLastPassError err
+
+printLastPassError :: LastPassError -> IO ()
+printLastPassError NotInstalled = putErrorLn "Error: LastPass CLI not installed. Please install with `brew install lastpass-cli`"
+printLastPassError NotLoggedIn = putErrorLn "Error: Not logged in. Please login with `lpass login`"
+printLastPassError ListPasswordsFailed = putErrorLn "Error: Failed to list passwords"
+printLastPassError (ListPasswordsParseFailed _) = putErrorLn "Error: Failed to parse list passwords output"
+printLastPassError (ShowPasswordFailed _) = putErrorLn "Error: Failed to show password"
 
 printUsage :: IO ()
 printUsage = do
   name <- Env.getProgName
-  TextIO.putStrLn ("Usage: " <> Text.pack name <> " <search>")
+  putErrorLn ("Usage: " <> Text.pack name <> " <search>")
+
+putErrorLn :: Text -> IO ()
+putErrorLn = TextIO.hPutStrLn stderr
